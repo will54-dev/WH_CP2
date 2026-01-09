00e06fe94e99a007a4417988690a1bd4aa660fbc
will54-dev <will.haggard@ucas-edn.net>
2026-01-09 10:37:21 -0700

    financial calculator

---
+++ individual projects/Financial Calculator.py
@@ -0,0 +1,37 @@
+#WH 2nd Financial Calculator
+
+# a function for the user interaction takes a list that is [] and a start_num that is False
+    #loop
+        #clear the screen
+        #if start_num is False
+            #loop throgh list as item
+                #show itme
+        #else
+            #set count to a copy of start_num
+            #loop throgh list as item
+                #show count : item
+                #add one to count
+        #set want to user input
+        #try 
+            #returning want+start_num as the list itme in list
+        #else
+            #if want in list
+                #return want
+            #else 
+                #show not on list
+
+# a function for savings 
+    #clear the screen
+        #set week/month to function for the user interaction with a list of weekly deposit and  monthly deposit. and with start_num as 1
+        #clear the screen
+        #loop
+
+
+
+
+
+
+        #y = p(1+r/n)^nt
+
+
+
