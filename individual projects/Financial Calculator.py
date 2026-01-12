cc2f349f342e9234f5670df59d83cec539c71963
will54-dev <will.haggard@ucas-edn.net>
2026-01-12 10:50:41 -0700

    financial calculator

--- individual projects/Financial Calculator.py
+++ individual projects/Financial Calculator.py
@@ -1,30 +1,52 @@
 #WH 2nd Financial Calculator
 
-# a function for the user interaction takes a list that is [] and a start_num that is False
+# a function for the user_interaction takes a start_num that is False, text, and a list that is []
     #loop
         #clear the screen
+        #show text
         #if start_num is False
+            #set offset to 0
             #loop throgh list as item
                 #show itme
         #else
+            #set offset to a copy of start_num
             #set count to a copy of start_num
             #loop throgh list as item
                 #show count : item
                 #add one to count
         #set want to user input
         #try 
-            #returning want+start_num as the list itme in list
+            #returning want-offset as the list itme in list
         #else
             #if want in list
                 #return want
             #else 
                 #show not on list
 
+# a functiom for user interaction whith intergers with text
+    #loop
+        #clear the screen
+        #show text
+        #user input
+        #try
+            #user input to an int
+        #else
+            #pass
+        #return user input
+
 # a function for savings 
     #clear the screen
-        #set week/month to function for the user interaction with a list of weekly deposit and  monthly deposit. and with start_num as 1
-        #clear the screen
-        #loop
+    #show Would you like weekly of monthly deposets.
+    #set weekly/monthly to user_interaction with 1 text Would you like weekly or monthly deposets? with week and month
+    #set deposit to user_intergers with How much are you depositing?
+    #set save_to to user_intergers with What are you saving to?
+    #clear the screen
+    #show it will take you {round save_to/deposit} {weekly/monthly} to save to {save_to}.
+    #user imput
+
+#a function for Compound Interest
+
+
 
 
 
---
+++ notes/lists.py
@@ -0,0 +1,23 @@
+# WH 2nd lists notes
+
+#lists
+numbers = [1,2,3,44,5,67,668]
+numbers[-1] = "4"
+numbers.append(2)
+print(numbers)
+
+# catagories, data storage, control randomization
+# list methods .append .pop .remove .sort len() .clear .copy .index .extend .reverse
+
+#Tuples
+
+white = (255,255,255)
+#white[1] = 0
+
+#unpack tuple
+red,green,blue = white
+print(red)
+
+#set
+letters = {"a","b","c","d"}
+print(letters)
