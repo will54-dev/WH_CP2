91e33f89b6f0bd7ec20cbbf078e1c07281662c7f
will54-dev <will.haggard@ucas-edu.net>
2026-01-28 19:43:23 +0000

    randome password genorator

---
+++ individual projects/Random Password Generator.py
@@ -0,0 +1,26 @@
+#WH 2nd passsword genorator
+
+#
+#
+#Functions for the different password requirements
+    # set the character list
+    #ask the user if they want to have uppercase letters
+    #if yes
+        #add the uppercase letters to the character list
+    #if no
+    #ask the user if they want to haav lowercase letters
+    #if yes
+        #add the uppercase letters to the character list
+    #if no
+    #ask the user if they want to have numbers
+    #if yes
+        #add the uppercase letters to the character list
+    #if no
+    #ask the user if they want to have special characters
+    #if yes 
+        #add the uppercase letters to the character list
+    #if no
+#main funtion for the menu
+
+
+match
