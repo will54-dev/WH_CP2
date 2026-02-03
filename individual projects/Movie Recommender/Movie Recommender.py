1f2a8417033d3aead48f4204ddad909b17cf3612
will54-dev <will.haggard@ucas-edu.net>
2026-02-03 19:45:20 +0000

    started the movie recommender

---
+++ individual projects/Movie Recommender/Movie Recommender.py
@@ -0,0 +1,7 @@
+# WH 2d movie recommedenr
+
+#import the csv library
+
+#function for returning the dictionary with the movies as keays that takes the file path
+    #opens the file with the locatio as read named movies
+        #set to a read of file with the csv reader
