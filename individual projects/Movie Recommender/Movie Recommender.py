4b70a087d2b208756b53c563d7f267a2bbcf809f
will54-dev <will.haggard@ucas-edu.net>
2026-02-04 19:43:50 +0000

    movie recoender

--- individual projects/Movie Recommender/Movie Recommender.py
+++ individual projects/Movie Recommender/Movie Recommender.py
@@ -4,4 +4,9 @@
 
 #function for returning the dictionary with the movies as keays that takes the file path
     #opens the file with the locatio as read named movies
-        #set to a read of file with the csv reader
+        #set content to a read of file with the csv reader
+        #set header to next in content
+        #set list_of_movies to a list
+        #loop for the item in content
+            #add a dictionary with the header then the item to the end of list_of_movies
+    #retuen the list_of_movies
